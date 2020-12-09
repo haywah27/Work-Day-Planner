7b5aa52337f0e0e4c2b3fe3573b926a02c7d3f22
haywah27 <hayleywahlroos15@gmail.com>
2020-12-08 17:01:39 -0800

    adding first time row

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+var saveButton = document.querySelector(".saveBtn");
 var dayEl = document.querySelector("#currentDay");
-// var NowMoment = moment();
-var date = moment().format('LTS');
+// day of the week, month, date
+var date = moment().format("dddd, MMMM Do");
 
-
+// display current date in header
 function displayCurrentDay(){
     dayEl.innerHTML = date;
 }
 
 displayCurrentDay();
+
+
+$(document).ready(function() { 
+
+    // console log if button clicked
+    $(".saveBtn").on("click", function () {
+        console.log("button clicked");
+    })
+    
+})
