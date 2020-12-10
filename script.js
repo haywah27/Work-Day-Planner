ad090962f7875291c654c9286906b72482da3f72
haywah27 <hayleywahlroos15@gmail.com>
2020-12-10 14:03:15 -0800

    adding color to rows based on current hour

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 var saveButton = document.querySelector(".saveBtn");
 var dayEl = document.querySelector("#currentDay");
-// day of the week, month, date
+// day of the week, month, day
 var date = moment().format("dddd, MMMM Do");
 
 // display current date in header
 function displayCurrentDay(){
-    dayEl.innerHTML = date;
+    // dayEl.innerHTML = date;
+    $(dayEl).text(date);
 }
 
 displayCurrentDay();
 
 
-$(document).ready(function() { 
+// set color of textbox based on hour
+function hourDisplay() {
+    // get current hour through moment.js
+    var currentHr = moment().hour();
 
-    // console log if button clicked
-    $(".saveBtn").on("click", function () {
-        console.log("button clicked");
+    // for each row associated with class timeBlock, do these things:
+    $(".time-block").each(function () {
+        // get id name and turn it into a readable number
+        var hourRow = parseInt($(this).attr("id"));
+        
+        // console.log(this)
+        // turn grey if current hour is greater than the hour row (we have moved past that hour block)
+        if (currentHr > hourRow) {
+            $(this).addClass("past");
+        }
+        
+        // turn red if its the current hour
+        else if (currentHr === hourRow) {
+            $(this).addClass("present");
+        }
+
+        // turn green if the current hour has not occured
+        else {
+            $(this).addClass("future");
+        }
     })
-    
-})
+}
+hourDisplay();
